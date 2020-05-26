8c610fa972ac6d119fe32c82b18ea0aaaa30409b
Aayushjn <aayushjn11@gmail.com>
2020-05-26 14:36:47 +0530

    feat(neo4j): Neo4j configuration and skeleton code

--- src/db/neo4j.py
+++ src/db/neo4j.py
@@ -1,3 +1,19 @@
 """
 Neo4j related DB calls
 """
+from typing import List, Dict
+
+from py2neo.database import Graph
+
+from src.settings import NEO4J_HOST, NEO4J_PORT, NEO4J_USER, NEO4J_PASS, NEO4J_SCHEME
+
+
+class Neo4j:
+    def __init__(self):
+        self.graph = Graph(scheme=NEO4J_SCHEME, host=NEO4J_HOST, port=NEO4J_PORT, auth=(NEO4J_USER, NEO4J_PASS))
+
+    def write_sql_to_neo(self, table: Dict[str, List[str]]):
+        pass
+
+    def write_nosql_to_neo(self, collection: Dict[str, Dict]):
+        pass
--- src/settings.py
+++ src/settings.py
@@ -13,3 +13,9 @@ MYSQL_CONFIG = {
 MONGO_HOST = os.getenv('MONGO_HOST', 'localhost')
 MONGO_PORT = os.getenv('MONGO_PORT', 27017)
 MONGO_DB_NAME = os.getenv('MONGO_DB_NAME')
+
+NEO4J_HOST = os.getenv('NEO4J_HOST', 'localhost')
+NEO4J_PORT = os.getenv('NEO4J_PORT', 7474)
+NEO4J_USER = os.getenv('NEO4J_USER', 'neo4j')
+NEO4J_PASS = os.getenv('NEO4J_PASS', 'neo4j')
+NEO4J_SCHEME = os.getenv('NEO4J_SCHEME', 'http')
