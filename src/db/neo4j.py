253f22b43e86de9536bd5bb9a2519ed922acd804
Aayushjn <aayushjn11@gmail.com>
2020-05-26 15:17:15 +0530

    feat(neo4j): Neo4j method changed
    Since Mongo and MySQL return the same data formats, the write method in Neo4j has been extracted to a common one

--- src/db/neo4j.py
+++ src/db/neo4j.py
@@ -12,8 +12,10 @@ class Neo4j:
     def __init__(self):
         self.graph = Graph(scheme=NEO4J_SCHEME, host=NEO4J_HOST, port=NEO4J_PORT, auth=(NEO4J_USER, NEO4J_PASS))
 
-    def write_sql_to_neo(self, table: Dict[str, List[str]]):
-        pass
-
-    def write_nosql_to_neo(self, collection: Dict[str, Dict]):
+    def write_records_to_neo(self, records: Dict[str, List[Dict[str, object]]]):
+        """
+        Records from a MySQL or Mongo database are stored as a graph in Neo4j
+        :param records: dictionary of entries as provided by :py:meth:`src.db.MySQL.extract_records_by_table or
+        :py:meth:`src.db.Mongo.extract_collections`
+        """
         pass
