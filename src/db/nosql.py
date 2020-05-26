aa84bb6ff6afa09a2bc9cd5174c88a092f89f075
Aayushjn <aayushjn11@gmail.com>
2020-05-26 14:54:47 +0530

    feat(nosql): Mongo collection entries are now fetched from the database and returned as a dictionary

--- src/db/nosql.py
+++ src/db/nosql.py
@@ -14,24 +14,36 @@ class Mongo:
     """
     def __init__(self):
         self.client = pymongo.MongoClient(f'mongodb://{MONGO_HOST}:{MONGO_PORT}/')
+        self.db = self.client[MONGO_DB_NAME]
 
     def extract_collection_details(self) -> Dict[str, List[str]]:
         """
         Fetches collections in the database and gets the keys of the collection
         :return: dictionary containing {collection_name: [key1, key2, ...]}
         """
-        db = self.client[MONGO_DB_NAME]
-        collection_names = db.list_collection_names()
+        collection_names = self.db.list_collection_names()
 
         details = {}
         for name in collection_names:
             details[name] = []
-            item = db[name].find_one()
+            item = self.db[name].find_one()
             for key in item:
                 if key != '_id':
                     details[name].append(key)
 
         return details
+
+    def extract_collections(self) -> Dict[str, List[Dict[str, object]]]:
+        details = self.extract_collection_details()
+
+        records = {}
+        for key in details:
+            objects = self.db[key].find()
+            records[key] = []
+            for record in objects:
+                records[key].append(record)
+
+        return records
 
     def close(self):
         """
