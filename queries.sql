9960bb98d9217d93781ffc10314e88a4a8db3091
Aayushjn <aayushjn11@gmail.com>
2020-06-12 17:41:40 +0530

    fix(misc): missing semicolon added

--- queries.sql
+++ queries.sql
@@ -2,4 +2,4 @@ SELECT p.* FROM physician p LIMIT 3;
 SELECT phy.name, pat.name FROM physician phy, patient pat WHERE phy.employeeid = pat.pcp;
 SELECT * FROM block;
 SELECT name, employeeid, ssn FROM nurse WHERE registered='t' or position = 'Nurse' ORDER BY name, employeeid;
-INSERT INTO patient VALUES(10, 'AJ', 'Somewhere', '1234567890', 1, 2)
+INSERT INTO patient VALUES(10, 'AJ', 'Somewhere', '1234567890', 1, 2);
